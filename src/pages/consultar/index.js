6eb8a1f5de61a7525591211b5b9df59fce2fd3f4
0ld3nburg <ceduburg@gmail.com>
2023-10-16 20:42:31 -0300

    ajustes

--- src/pages/consultar/index.js
+++ src/pages/consultar/index.js
@@ -1,8 +1,9 @@
 let bntList = document.getElementById('bnt-list');
 
 bntList.addEventListener('click', async () => {
-  let response = await fetch(`http://localhost:4000/batata`);
-  let infos = await response.json();
-  console.log(infos)
-  return infos;
+let response = await fetch(`http://localhost:4000/batata`);
+let infos = await response.json();
+console.log(infos)
+return infos;
 });
+
